6651438432a9898eeaf31e6903e2722fba7429f6
Mike <rodgerrw@yandex.ru>
2018-08-16 14:05:08 +0300

    test1

---
+++ 1/test.rb
@@ -0,0 +1,12 @@
+puts "What's your first name? "
+first_name = gets.chomp.capitalize!
+puts first_name
+puts "What's your last name? "
+last_name = gets.chomp.capitalize!
+puts last_name
+puts "What's your city? "
+city = gets.chomp.capitalize!
+puts city
+puts "What's your state? "
+state = gets.chomp.upcase
+puts state
