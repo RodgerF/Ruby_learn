6d34a586b6d529138d469b6396fbe7f63e008454
Mike <rodgerrw@yandex.ru>
2019-03-26 19:14:00 +0300

    3 решения  на добавления элементов в массив с шагом

--- 2/array.rb
+++ 2/array.rb
@@ -1,13 +1,25 @@
 array = (10..100).to_a
 array.delete_if {|x|  x % 5 != 0 }
-print array
-puts " "
+p array
+puts " ++++ "
 
-a = []
-range = (10..100)
-range.step(5)
+mas = []
+i = 5
+while i < 100
+  i += 5
+  mas << i
+end
+p mas
+puts "<<<<<<<<<<<<<<<<<"
 
-print range
+arr = []
+rang = (10..100)
+rang.step(5) do |a|
+  arr.push (a)
+end
+p  arr
+
+
 
 
 
