6bfde849ae714f5a8da62d3533c00c47fa188dff
Mike <rodgerrw@yandex.ru>
2020-03-01 01:29:05 +0300

    test

---
+++ test.rb
@@ -0,0 +1,17 @@
+a = [1, 2, 3]*2
+b = [2, 3, 4]
+p a
+res = a | b
+p res
+
+items =["place", "key","holder"]
+items[1,0]= ["bottle", "my"]
+print items
+
+puts items[-3], items[-2], items[-1]
+
+puts "sololearn".delete("sol")<<"cash"
+
+array = [1,2]
+t = array * array
+p t
